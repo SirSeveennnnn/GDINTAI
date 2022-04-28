cb17525c40241bcc224e725955827b53d1e841d6
Miiiiroooo <aamirakim23.mir@gmail.com>
2022-04-29 03:26:30 +0800

    added new GameOver Canvas
    GameOver window added with no functionality

---
+++ Assets/Scripts/GameOverScript.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class GameOverScript : MonoBehaviour
+{
+
+    public void ChangeToScene(int sceneToChangeTo)
+    {
+        Application.LoadLevel(sceneToChangeTo);
+    }
+
+}
